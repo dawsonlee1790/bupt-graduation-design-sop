16fe76af789ba6e71f2d5b48165a23ed5e29e45a
dawsonlee1790 <dawsonlee1790@outlook.com>
2019-05-15 01:26:24 +0800

    添加新类

--- src/main/java/bupt/dawsonlee1790/sop/service/MakeSopService.java
+++ src/main/java/bupt/dawsonlee1790/sop/service/MakeSopService.java
@@ -1,10 +1,18 @@
 package bupt.dawsonlee1790.sop.service;
 
+import bupt.dawsonlee1790.sop.entity.Sop;
+import bupt.dawsonlee1790.sop.repopsitory.SopRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MakeSopService {
 
+    @Autowired
+    private SopRepository sopRepository;
 
+    public void makeSop(Sop sop) {
+        sopRepository.save(sop);
+    }
 
 }
