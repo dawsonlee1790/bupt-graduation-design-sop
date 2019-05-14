50c2b76bae2dd1c47405ff4329ebdb0b92204ab3
dawsonlee1790 <dawsonlee1790@outlook.com>
2019-05-15 01:27:40 +0800

    修改类

--- src/main/java/bupt/dawsonlee1790/sop/controller/MakeSopController.java
+++ src/main/java/bupt/dawsonlee1790/sop/controller/MakeSopController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/MakeSopController")
 public class MakeSopController {
 
-
     @Autowired
     public MakeSopService makeSopService;
 
