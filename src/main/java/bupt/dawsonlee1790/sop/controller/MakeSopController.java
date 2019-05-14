a905e1648822e3085aa541f5d1dc7c58b74fd647
dawsonlee1790 <dawsonlee1790@outlook.com>
2019-05-15 01:27:01 +0800

    修改类

--- src/main/java/bupt/dawsonlee1790/sop/controller/MakeSopController.java
+++ src/main/java/bupt/dawsonlee1790/sop/controller/MakeSopController.java
@@ -1,9 +1,10 @@
 package bupt.dawsonlee1790.sop.controller;
 
-import bupt.dawsonlee1790.sop.repopsitory.SopRepository;
 import bupt.dawsonlee1790.sop.entity.Sop;
+import bupt.dawsonlee1790.sop.service.MakeSopService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,12 +13,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/MakeSopController")
 public class MakeSopController {
 
+
     @Autowired
-    private SopRepository sopRepository;
+    public MakeSopService makeSopService;
 
     @PutMapping("/make")
-    public void makeSop(@RequestBody Sop sop) {
-        sopRepository.save(sop);
+    public void makeSop(@RequestBody @Validated  Sop sop) {
+        makeSopService.makeSop(sop);
     }
 
 }
