01486481fa84acc911dc86cd8272487536dd4125
dawsonlee1790 <dawsonlee1790@outlook.com>
2019-05-18 03:12:39 +0800

    迭代二

--- src/main/java/bupt/dawsonlee1790/sop/controller/MakeProductionPlanController.java
+++ src/main/java/bupt/dawsonlee1790/sop/controller/MakeProductionPlanController.java
@@ -1,13 +1,7 @@
 package bupt.dawsonlee1790.sop.controller;
 
-import bupt.dawsonlee1790.sop.repopsitory.ProductionOrderRepository;
-import bupt.dawsonlee1790.sop.repopsitory.ProductionPlanRepository;
-import bupt.dawsonlee1790.sop.repopsitory.SopRepository;
 import bupt.dawsonlee1790.sop.dto.MakePlanDTO;
-import bupt.dawsonlee1790.sop.entity.ProductionOrder;
-import bupt.dawsonlee1790.sop.entity.ProductionPlan;
-import bupt.dawsonlee1790.sop.entity.Sop;
-import bupt.dawsonlee1790.sop.entity.SopStep;
+import bupt.dawsonlee1790.sop.service.MakeProductionPlanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -19,41 +13,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MakeProductionPlanController {
 
     @Autowired
-    private ProductionPlanRepository productionPlanRepository;
-
-    @Autowired
-    private SopRepository sopRepository;
-
-    @Autowired
-    private ProductionOrderRepository productionOrderRepository;
-
+    private MakeProductionPlanService makeProductionPlanService;
 
     @PutMapping("/make")
     public void makePlan(@RequestBody MakePlanDTO makePlanDTO) {
-        Sop sop = sopRepository.getOne(makePlanDTO.getSopId());
-        ProductionPlan productionPlan = new ProductionPlan();
-        productionPlan.setName(makePlanDTO.getPlanName());
-        productionPlan.setNumber(makePlanDTO.getNumber());
-        productionPlan.setPlanningStartDate(makePlanDTO.getStartDate());
-        productionPlan.setPlanningEndDate(makePlanDTO.getEndDate());
-        productionPlan.setResponsible(makePlanDTO.getResponsible());
-        productionPlan.setStatus("待审核");
-        ProductionOrder startOrder = dfs(sop.getStartStep());
-        productionPlan.setStartOrder(startOrder);
-        productionPlanRepository.save(productionPlan);
+        makeProductionPlanService.makePlan(makePlanDTO);
     }
-
-    private ProductionOrder dfs(SopStep sopStep) {
-        SopStep next = sopStep.getNext();
-        if (next == null) return null;
-        ProductionOrder nextOrder = dfs(sopStep.getNext());
-        ProductionOrder productionOrder = new ProductionOrder();
-        productionOrder.setOperationContent(sopStep.getOperationContent());
-        productionOrder.setExecutor(sopStep.getExecutor());
-        productionOrder.setNext(nextOrder);
-        productionOrderRepository.save(productionOrder);
-        return productionOrder;
-    }
-
 
 }
---
+++ src/main/java/bupt/dawsonlee1790/sop/service/MakeProductionPlanService.java
@@ -0,0 +1,55 @@
+package bupt.dawsonlee1790.sop.service;
+
+import bupt.dawsonlee1790.sop.dto.MakePlanDTO;
+import bupt.dawsonlee1790.sop.entity.ProductionOrder;
+import bupt.dawsonlee1790.sop.entity.ProductionPlan;
+import bupt.dawsonlee1790.sop.entity.Sop;
+import bupt.dawsonlee1790.sop.entity.SopStep;
+import bupt.dawsonlee1790.sop.repopsitory.ProductionOrderRepository;
+import bupt.dawsonlee1790.sop.repopsitory.ProductionPlanRepository;
+import bupt.dawsonlee1790.sop.repopsitory.SopRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
+public class MakeProductionPlanService {
+
+
+    @Autowired
+    private ProductionPlanRepository productionPlanRepository;
+
+    @Autowired
+    private SopRepository sopRepository;
+
+    @Autowired
+    private ProductionOrderRepository productionOrderRepository;
+
+    public void makePlan(MakePlanDTO makePlanDTO) {
+        Sop sop = sopRepository.getOne(makePlanDTO.getSopId());
+        ProductionPlan productionPlan = new ProductionPlan();
+        productionPlan.setName(makePlanDTO.getPlanName());
+        productionPlan.setNumber(makePlanDTO.getNumber());
+        productionPlan.setPlanningStartDate(makePlanDTO.getStartDate());
+        productionPlan.setPlanningEndDate(makePlanDTO.getEndDate());
+        productionPlan.setResponsible(makePlanDTO.getResponsible());
+        productionPlan.setStatus("待审核");
+        ProductionOrder startOrder = dfs(sop.getStartStep());
+        productionPlan.setStartOrder(startOrder);
+        productionPlanRepository.save(productionPlan);
+    }
+
+    private ProductionOrder dfs(SopStep sopStep) {
+        SopStep next = sopStep.getNext();
+        if (next == null) return null;
+        ProductionOrder nextOrder = dfs(sopStep.getNext());
+        ProductionOrder productionOrder = new ProductionOrder();
+        productionOrder.setOperationContent(sopStep.getOperationContent());
+        productionOrder.setExecutor(sopStep.getExecutor());
+        productionOrder.setNext(nextOrder);
+        productionOrderRepository.save(productionOrder);
+        return productionOrder;
+    }
+
+}
