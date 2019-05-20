ab661cb63c846069bdf634d67207a434cd44c08c
dawsonlee1790 <dawsonlee1790@outlook.com>
2019-05-20 11:51:47 +0800

    用例执行生产指令添加权限认证注解

--- src/main/java/bupt/dawsonlee1790/sop/controller/ExecuteOrderController.java
+++ src/main/java/bupt/dawsonlee1790/sop/controller/ExecuteOrderController.java
@@ -1,5 +1,7 @@
 package bupt.dawsonlee1790.sop.controller;
 
+import bupt.dawsonlee1790.sop.certification.Actor;
+import bupt.dawsonlee1790.sop.certification.Role;
 import bupt.dawsonlee1790.sop.entity.ProductionPlan;
 import bupt.dawsonlee1790.sop.service.ExecuteOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,16 +23,18 @@ public class ExecuteOrderController {
     private ExecuteOrderService executeOrderService;
 
     @GetMapping("/")
+    @Actor({Role.Forklift, Role.WorkshopManager})
     public List<ProductionPlan> getPlanList() {
         return executeOrderService.getPlanList();
     }
 
     @PostMapping("/{planId}")
+    @Actor({Role.Forklift, Role.WorkshopManager})
     public String executeOrder(@PathVariable("planId") long planId, HttpServletResponse response) {
         try {
             executeOrderService.executeOrder(planId);
             return "";
-        }catch (Exception e){
+        } catch (Exception e) {
             response.setStatus(HttpStatus.BAD_REQUEST.value());
             return e.getMessage();
         }
