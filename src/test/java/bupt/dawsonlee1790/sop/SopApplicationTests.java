890f3beaaaf6dd978938bf06d92b098616f59d7a
dawsonlee1790 <dawsonlee1790@outlook.com>
2019-05-20 13:14:15 +0800

    测试的项目结构更新

--- src/test/java/bupt/dawsonlee1790/sop/SopApplicationTests.java
+++ src/test/java/bupt/dawsonlee1790/sop/SopApplicationTests.java
@@ -1,32 +1,98 @@
 package bupt.dawsonlee1790.sop;
 
-import bupt.dawsonlee1790.sop.controller.MakeProductionPlanControllerTest;
-import bupt.dawsonlee1790.sop.controller.MakeSopControllerTest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.text.SimpleDateFormat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SopApplicationTests {
 
-    private MakeSopControllerTest makeSopControllerTest;
-    private MakeProductionPlanControllerTest makeProductionPlanControllerTest;
+    @Autowired
+    private WebApplicationContext webApplicationContext;
 
+    private MockMvc mockMvc;
+
+    private ObjectMapper mapper;
 
     @Before
-    public void init(){
-        makeSopControllerTest = new MakeSopControllerTest();
-        makeProductionPlanControllerTest = new MakeProductionPlanControllerTest();
+    public void init() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+        mapper = new ObjectMapper();
     }
-
 
     @Test
     public void contextLoads() throws Exception {
-        makeSopControllerTest.makeSop();
-        makeProductionPlanControllerTest.makeProductionPlan();
+        this.makeSop();
+        this.makeProductionPlan();
     }
+
+
+    private void makeSop() throws Exception {
+        String url = "/MakeSopController/make";
+        //language=JSON
+        String content = "{\n" +
+                "  \"name\": \"NewMedicine\",\n" +
+                "  \"number\": 100,\n" +
+                "  \"startStep\": {\n" +
+                "    \"operationContent\": \"操作1\",\n" +
+                "    \"executorGroup\": \"Forklift\",\n" +
+                "    \"next\": {\n" +
+                "      \"operationContent\": \"操作2\",\n" +
+                "      \"executorGroup\": \"WorkshopManager\"\n" +
+                "    }\n" +
+                "  }\n" +
+                "}";
+        MvcResult mvcResult = put(url, content);
+        Assert.assertEquals(200, mvcResult.getResponse().getStatus());
+    }
+
+    private void makeProductionPlan() throws Exception {
+        String url = "/MakeProductionPlanController/make";
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        //language=JSON
+        String content = "{\n" +
+                "  \"sopId\": 1,\n" +
+                "  \"startDate\": \"2019-04-19\",\n" +
+                "  \"endDate\": \"2019-05-20\",\n" +
+                "  \"number\": 100,\n" +
+                "  \"planName\": \"New药批次100\",\n" +
+                "  \"responsible\": \"生产责任人-339\"\n" +
+                "}";
+        MvcResult mvcResult = put(url, content);
+        Assert.assertEquals(200, mvcResult.getResponse().getStatus());
+    }
+
+    private MvcResult put(String url, String content) throws Exception {
+        return mockMvc.perform(
+                MockMvcRequestBuilders.put(url).content(content).contentType(MediaType.APPLICATION_JSON)
+        ).andReturn();
+    }
+
+    private MvcResult post(String url, String content) throws Exception {
+        return mockMvc.perform(
+                MockMvcRequestBuilders.post(url).content(content).contentType(MediaType.APPLICATION_JSON)
+        ).andReturn();
+    }
+
+    private MvcResult get(String url) throws Exception {
+        return mockMvc.perform(
+                MockMvcRequestBuilders.post(url).contentType(MediaType.APPLICATION_JSON)
+        ).andReturn();
+    }
+
 
 }
