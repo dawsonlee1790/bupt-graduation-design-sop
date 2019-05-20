7fd823f9f2bc93fee843756040d49a82cbf690f5
dawsonlee1790 <dawsonlee1790@outlook.com>
2019-05-20 12:08:56 +0800

    修改bug

--- src/test/java/bupt/dawsonlee1790/sop/controller/MakeSopControllerTest.java
+++ src/test/java/bupt/dawsonlee1790/sop/controller/MakeSopControllerTest.java
@@ -6,6 +6,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -15,6 +17,7 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -27,8 +30,12 @@ public class MakeSopControllerTest {
 
     private ObjectMapper mapper;
 
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+
     @Before
     public void init() {
+        logger.info("");
         mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
         mapper = new ObjectMapper();
     }
